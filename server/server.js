a285ca39d36831ace1477ecdd1102ba3db330820
kolobot <nedol@narod.ru>
2024-02-03 22:11:25 +0100

    ws 3000

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import https from 'https';
 
 import http from 'http';
@@ -9,7 +11,9 @@ import url from 'url';
 
 // let con_param = globaljs.con_param; //change every 8 min
 
-export default function HandleRequest(req, q, res) {}
+export default function HandleRequest(req, q, res) {
+	console.log(q);
+}
 
 // wget https://nodejs.org/dist/v9.7.0/node-v9.7.0-linux-x64.tar.x
 //     tar xJf node-v9.7.0-linux-x64.tar.xz --strip 1
