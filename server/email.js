11095ccee0621438d47438af997a776933636954
nedol <nedooleg@gmail.com>
2024-10-19 23:28:59 +0200

    new ws

---
+++ server/email.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import nodemailer from 'nodemailer';
+
+export default class Email {
+  constructor() {
+    // this.transporter = nodemailer.createTransport({
+    //     service: 'yandex',
+    //     auth: {
+    //         user: 'nedol@yandex.ru', //"admin@delivery-angels.ru",//
+    //         pass: 'Nissan@386'
+    //     }
+    // })
+    // this.transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: 'nedol.d2d@gmail.com', //"admin@delivery-angels.ru",//
+    //         pass: 'Nissan_386'
+    //     }
+    // })
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail',
+      port: 465,
+      secure: true, // upgrade later with STARTTLS
+      auth: {
+        user: 'nedooleg@gmail.com',
+        // pass: 'NissanPathfinder@720'
+        pass: 'gytn jkgk ucll koig',
+      },
+    });
+  }
+
+  SendMail(from, to, subj, html, cb) {
+    let mailOptions = {
+      from: from, //from, //'youremail@gmail.com',
+      to: to, //'myfriend@yahoo.com',
+      subject: subj,
+      html: html,
+    };
+    this.transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        cb({ err: error });
+        console.log(error);
+      } else {
+        cb('Email sent: ' + info.response);
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
+};
